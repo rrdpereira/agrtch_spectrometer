460cedc1ec479f92026fa78f2834ad73f23aa86f
rrdpereira <robsondutra.pereira@outlook.co@outlook.com>
2023-02-28 10:17:58 -0300

    add: lib implementation

---
+++ spectrometer_lib_FLMS04868.py
@@ -0,0 +1,42 @@
+from seabreeze.spectrometers import list_devices, Spectrometer
+from re import search
+import matplotlib.pyplot as plt
+import numpy as np
+
+import sys, time, os, datetime
+from platform import python_version
+
+print(f"(Sys version) :|: {sys.version} :|:")
+os.system("which python")
+print(f"(Python version) :#: {python_version()} :#:")
+
+Dots1Inch_height=96
+Dots1Inch_width=96
+
+if __name__ == '__main__':
+    SerialFLMS='FLMS04868'
+    specs=Spectrometer.from_serial_number(SerialFLMS)
+    print(f"(Spectrometer) :: {specs} ::")
+
+    specs.integration_time_micros(5000)
+
+    wavelen=specs.wavelengths()
+    print(f"(Wavelengths) :: {wavelen} ::")
+
+    intens=specs.intensities()
+    print(f"(Intensities) :: {intens} ::")
+
+    # plot the acquired spectrum
+    plt.ion()
+    try:
+        while True:
+            wave,ints=specs.spectrum()
+            np.savetxt(time.strftime("%Y%m%d_%H%M%S")+"_"+SerialFLMS+".csv", np.vstack((wave,ints)).T, delimiter=', ')
+            plt.title(SerialFLMS+"_"+time.strftime("%Y%m%d_%H%M%S"))
+            plt.plot(wave,ints)
+            plt.pause(5)
+            plt.savefig(time.strftime("%Y%m%d_%H%M%S")+"_"+SerialFLMS+".png",format="png",dpi=Dots1Inch_height)
+            plt.cla()
+    except KeyboardInterrupt:
+        # Exit on CTRL-C
+        pass            
--- tests/cxgsadf.py
+++ tests/cxgsadf.py
@@ -1,14 +1,48 @@
 from seabreeze.spectrometers import list_devices, Spectrometer
 from re import search
+import matplotlib.pyplot as plt
+import numpy as np
 
-devices = str( list_devices())
-print(devices)
+import sys, time, os, datetime
+os.system("which python")
+print(sys.version)
+from platform import python_version
+print(python_version())
 
-substring = "4868"
-if search(substring, devices):
-    print ("FLMS04868")
-else:
-    print ("FLMS04767")
+Dots1Inch_height=96
+Dots1Inch_width=96
+
+# Spectrometer.open()
+# devices = str( list_devices())
+# print(devices)
+
+specs=Spectrometer.from_serial_number('FLMS04868')
+print(specs)
+
+specs.integration_time_micros(5000)
+
+wavelen=specs.wavelengths()
+print(wavelen)
+
+intens=specs.intensities()
+print(intens)
+
+wave,ints=specs.spectrum()
+
+np.savetxt(time.strftime("%Y%m%d_%H%M%S")+"_FLMS04727"+".csv", np.vstack((wave,ints)).T, delimiter=', ')
+plt.title("FLMS04727_"+time.strftime("%Y%m%d_%H%M%S"))
+plt.plot(wave,ints)
+plt.pause(5)
+plt.savefig(time.strftime("%Y%m%d_%H%M%S")+"_FLMS04727"+".png",format="png",dpi=Dots1Inch_height)
+plt.cla()
+
+# Spectrometer.close()
+
+# substring = "4868"
+# if search(substring, devices):
+#     print ("FLMS04868")
+# else:
+#     print ("FLMS04767")
 
 
 
